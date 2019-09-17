cc44cfd0bc1195df3c2045615e9f34311a18b727
Bill Ni <bni@stuy.edu>
2019-09-17 16:56:35 -0400

    fix iframe

---
+++ v1/js/iframemod/resize.js
@@ -0,0 +1,5 @@
+document.getElementById("frame").style.height = window.innerHeight * 0.85;
+
+$(window).resize(function() {
+  document.getElementById("frame").style.height = window.innerHeight * 0.85;
+});
