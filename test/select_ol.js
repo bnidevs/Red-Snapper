20e06bd846381f252d8c3cb9def586a405ee7e6c
Bill Ni <bni@stuy.edu>
2019-10-01 21:37:08 -0400

    bug control for box highlighting js

--- test/select_ol.js
+++ test/select_ol.js
@@ -1,13 +1,15 @@
-var allel = document.getElementsByClassName("brdr-hl");
-
-var el;
-
-for(var i = 0; i < allel.length; i++){
-	allel[i].addEventListener("click", refcs);
-}
+var el = document.body;
 
 var refcs = (e) => {
-	el.style.outline = "";
+	el.style.outline = null;
 	el = e.target;
 	el.style.outline = "solid 3px black !important";
+}
+
+window.onload = function() {
+	var allel = document.getElementsByClassName("brdr-hl");
+
+	for(var i = 0; i < allel.length; i++){
+		allel[i].addEventListener("click", function(e){refcs(e)});
+	}
 }
