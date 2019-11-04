5c95e27dd0e0476cc61ae33ecd9b13d3d8c653af
Bill Ni <bni@stuy.edu>
2019-11-04 03:13:48 -0500

    implemented test files into v1

--- test/select_ol.js
+++
@@ -1,7 +0,0 @@
-for (const element of document.querySelectorAll('.brdr-hl')) {
-  element.addEventListener('click', () => {
-    element.classList.contains('brdr-sl')
-      ? element.classList.replace('brdr-sl', 'brdr-none')
-      : element.classList.add('brdr-sl');
-  });
-}
