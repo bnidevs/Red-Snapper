ef1053b0da2d8c8c3134f05ec05e605a3c270278
Bill Ni <bni@stuy.edu>
2019-11-04 18:41:28 -0500

    begin on draggable div

--- v1/js/dragsys/drag.js
+++ v1/js/dragsys/drag.js
@@ -8,11 +8,17 @@ async function drag(ev) {
 
 	if(ev.target.id == "navdraggable"){
 
-		var resp = await fetch('https://raw.githubusercontent.com/bnidevs/Red-Snapper/master/v1/js/insertions/layout/navbar.html');
-		resp = await resp.text();
+		// var resp = await fetch('https://raw.githubusercontent.com/bnidevs/Red-Snapper/master/v1/js/insertions/layout/navbar.html');
+		// resp = await resp.text();
 
-		// ev.dataTransfer.setData("text", '<nav class="navbar navbar-expand-lg brdr-hl"><p class="navbar-brand">E Pluribus Unum</p></nav>');
-		ev.dataTransfer.setData("text", resp);
+		ev.dataTransfer.setData("text", '<nav class="navbar navbar-expand-lg brdr-hl"><p class="navbar-brand">E Pluribus Unum</p></nav>');
+		//ev.dataTransfer.setData("text", resp);
+		carry = true;
+		dtm();
+
+	}else if(ev.target.id == "divdraggable"){
+
+		ev.dataTransfer.setData("text", '<div class="row"><div class="col-12"><p>Doggo ipsum puggorino most angery pupper I have ever seen borkf heckin good boys, lotsa pats. He made many woofs corgo fluffer fat boi the neighborhood pupper borkdrive, you are doing me a frighten ruff pupperino borking doggo. Long bois adorable doggo puggorino wow very biscit shibe waggy wags shibe, heckin good boys tungg very hand that feed shibe extremely cuuuuuute borking doggo. adorable doggo fluffer. Shooberino doggorino blep smol shooberino wrinkler, super chub doge waggy wags sub woofer. Boof maximum borkdrive many pats h*ck yapper, long doggo vvv many pats. Noodle horse adorable doggo heckin good boys, corgo.</p></div></div>');
 		carry = true;
 		dtm();
 
@@ -25,10 +31,13 @@ function drop(ev) {
 	var data = ev.dataTransfer.getData("text");
 	iframedoc = document.getElementById("frame").contentDocument || document.getElementById("frame").contentWindow.document;
 	iframedoc.body.insertAdjacentHTML('beforeend', data);
-	iframedoc.body.lastChild.addEventListener('click', () => {
-    	iframedoc.body.lastChild.classList.contains('brdr-sl')
-      	? iframedoc.body.lastChild.classList.replace('brdr-sl', 'brdr-none')
-      	: iframedoc.body.lastChild.classList.add('brdr-sl');
+
+	var that = iframedoc.body.lastChild;
+
+	that.addEventListener('click', () => {
+    	that.classList.contains('brdr-sl')
+      	? that.classList.replace('brdr-sl', 'brdr-none')
+      	: that.classList.add('brdr-sl');
   	});
 	carry = false;
 	dtm();
