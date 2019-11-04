5c28a63cc1e3de3c7cfbe5750519fcfb0d36363f
Bill Ni <bni@stuy.edu>
2019-11-04 03:03:44 -0500

    finished drag and drop for navbar, will proceed with section and split, still working on hover system in iframe

--- v1/js/dragsys/drag.js
+++ v1/js/dragsys/drag.js
@@ -1,39 +1,42 @@
-// allowDrop doesn't trigger when navigation bar is dragged over the iframe
+var carry = false;
+
 function allowDrop(ev) {
-	console.log("over");
   	ev.preventDefault();
 }
 
-// drag works and is triggered when navigation bar is dragged
 function drag(ev) {
+
 	if(ev.target.id == "navdraggable"){
-		console.log("triggered");
 
-		fetch('https://raw.githubusercontent.com/bnidevs/Red-Snapper/master/v1/js/iframemod/layoutadd/navbar.html')
-		.then(response => response.text())
-		.then(function(response){
-			console.log(response);
-			ev.dataTransfer.setData("text/html", response);
-		})
-		.catch(error => console.error('Error:', error));
+		// fetch('https://raw.githubusercontent.com/bnidevs/Red-Snapper/master/v1/js/iframemod/layoutadd/navbar.html')
+		// .then(response => response.text())
+		// .then(function(response){
+		// 	console.log(response);
+		// 	ev.dataTransfer.setData("text", response);
+		// }).then(function(){
+		// 	return;
+		// })
+		// .catch(error => console.error('Error:', error));
 
-		console.log("dataTransfer successful");
-
-		return;
+		ev.dataTransfer.setData("text", '<nav class="navbar navbar-expand-lg brdr-hl"><p class="navbar-brand">E Pluribus Unum</p></nav>');
+		carry = true;
+		dtm();
 	}
 
-	ev.dataTransfer.setData("text/html", "temp");
 }
 
-// drop doesn't trigger when navigation bar is dragged over the iframe
 function drop(ev) {
-	console.log("dropped");
 	ev.preventDefault();
-	var data = ev.dataTransfer.getData("text/html");
-	iframedoc = ev.target.contentDocument || ev.target.contentWindow.document;
+	var data = ev.dataTransfer.getData("text");
+	iframedoc = document.getElementById("frame").contentDocument || document.getElementById("frame").contentWindow.document;
 	iframedoc.body.insertAdjacentHTML('beforeend', data);
+	carry = false;
 }
 
-document.getElementById("frame").addEventListener('ondrop', drop, false);
-document.getElementById("frame").addEventListener('ondragenter', allowDrop);
-document.getElementById("frame").addEventListener('ondragover', allowDrop);
+function dtm(){
+	if(carry){
+		document.getElementById("drop_dest").style.pointerEvents = "auto";
+	}else{
+		document.getElementById("drop_dest").style.pointerEvents = "none";
+	}
+}
