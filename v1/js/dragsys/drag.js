7cc9517952c7b186cf12ffd674dca84d31cea612
Bill Ni <bill.8ni@gmail.com>
2019-11-12 15:48:41 -0500

    Update drag.js

--- v1/js/dragsys/drag.js
+++ v1/js/dragsys/drag.js
@@ -12,12 +12,14 @@ function drag(ev) {
 		// resp = await resp.text();
 
 		ev.dataTransfer.setData("text", '<nav class="navbar navbar-expand-lg brdr-hl"><p class="navbar-brand">E Pluribus Unum</p></nav>');
+		ev.dataTransfer.setData("type", "navdraggable");
 		//ev.dataTransfer.setData("text", resp);
 
 	}else if(ev.target.id == "divdraggable"){
 
 		ev.dataTransfer.setData("text", '<div class="row brdr-hl"><div class="col-1"></div><div class="col-10"><p>Doggo ipsum puggorino most angery pupper I have ever seen borkf heckin good boys, lotsa pats. He made many woofs corgo fluffer fat boi the neighborhood pupper borkdrive, you are doing me a frighten ruff pupperino borking doggo. Long bois adorable doggo puggorino wow very biscit shibe waggy wags shibe, heckin good boys tungg very hand that feed shibe extremely cuuuuuute borking doggo. adorable doggo fluffer. Shooberino doggorino blep smol shooberino wrinkler, super chub doge waggy wags sub woofer. Boof maximum borkdrive many pats h*ck yapper, long doggo vvv many pats. Noodle horse adorable doggo heckin good boys, corgo.</p></div><div class="col-1"></div></div>');
-
+		ev.dataTransfer.setData("type", "divdraggable");
+		
 	}
 
   	carry = true;
@@ -45,7 +47,19 @@ function drop(ev) {
   	});
 
   	that.addEventListener('dblclick', () => {
-  		
+  		if(ev.dataTransfer.getData("type") == "navdraggable"){
+			var tl = that.getElementsByTagName('p');
+			for(var i = 0; i < tl.length; i++){
+				var t = tl[i].innerHTML;
+				tl[i].innerHTML = '<input type="text">';
+				tl[i].childNodes[0].value = t;
+			}
+		}else{
+			var tl = that.getElementsByTagName('p');
+			for(var i = 0; i < tl.length; i++){
+				tl[i].innerHTML = '<textarea>' + tl[i].innerHTML + '</textarea>';
+			}
+		}
   	});
 
 	carry = false;
