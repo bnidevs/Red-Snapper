1f5639e83b7fa9826cd5c1a5265e555c3b17f3d2
Bill Ni <bni@stuy.edu>
2019-11-16 19:11:06 -0500

    change color options to use the <input color> tag instead

--- v1/js/sidebarmod/sb_funcs.js
+++ v1/js/sidebarmod/sb_funcs.js
@@ -1,8 +1,3 @@
-var r_bg = 255, g_bg = 255, b_bg = 255;
-var r_tx = 0, g_tx = 0, b_tx = 0;
-
-var placement = true;
-
 window.onload = function(){
 	document.getElementById("defsb").addEventListener("click", defaultreplace);
 	document.getElementById("sblayout").addEventListener("click", layoutreplace);
@@ -67,89 +62,6 @@ var open_prev = () => {
 	prev_content.childNodes[0].remove();
 	new_win.document.head.innerHTML = iframedoc.head.innerHTML;
 	new_win.document.body.innerHTML = prev_content.innerHTML;
-}
-
-var update_placement = () => {
-	placement = !placement;
-	update('red', true, true);
-	update('green', true, true);
-	update('blue', true, true);
-}
-
-var update = (s, b, placement_changed = false) => {
-	if(s == 'red'){
-
-		if(placement_changed){
-			document.getElementById("r_slider").value = (placement ? r_bg : r_tx);
-			document.getElementById("r_value").value = (placement ? r_bg : r_tx);
-		}
-
-		if(b){
-			if(placement){
-				r_bg = document.getElementById("r_value").value;
-			}else{
-				r_tx = document.getElementById("r_value").value;
-			}
-			document.getElementById("r_slider").value = document.getElementById("r_value").value;
-		}else{
-			if(placement){
-				r_bg = document.getElementById("r_slider").value;
-			}else{
-				r_tx = document.getElementById("r_slider").value;
-			}
-			document.getElementById("r_value").value = document.getElementById("r_slider").value;
-		}
-	}
-
-	if(s == 'blue'){
-
-		if(placement_changed){
-			document.getElementById("b_slider").value = (placement ? b_bg : b_tx);
-			document.getElementById("b_value").value = (placement ? b_bg : b_tx);
-		}
-
-		if(b){
-			if(placement){
-				b_bg = document.getElementById("b_value").value;
-			}else{
-				b_tx = document.getElementById("b_value").value;
-			}
-			document.getElementById("b_slider").value = document.getElementById("b_value").value;
-		}else{
-			if(placement){
-				b_bg = document.getElementById("b_slider").value;
-			}else{
-				b_tx = document.getElementById("b_slider").value;
-			}
-			document.getElementById("b_value").value = document.getElementById("b_slider").value;
-		}
-	}
-
-	if(s == 'green'){
-
-		if(placement_changed){
-			document.getElementById("g_slider").value = (placement ? g_bg : g_tx);
-			document.getElementById("g_value").value = (placement ? g_bg : g_tx);
-		}
-
-		if(b){
-			if(placement){
-				g_bg = document.getElementById("g_value").value;
-			}else{
-				g_tx = document.getElementById("g_value").value;
-			}
-			document.getElementById("g_slider").value = document.getElementById("g_value").value;
-		}else{
-			if(placement){
-				g_bg = document.getElementById("g_slider").value;
-			}else{
-				g_tx = document.getElementById("g_slider").value;
-			}
-			document.getElementById("g_value").value = document.getElementById("g_slider").value;
-		}
-	}
-
-	document.getElementById("clr_view").backgroundColor = 'rgb(' + (placement ? r_bg : r_tx) + ',' + (placement ? b_bg : b_tx) + ',' + (placement ? g_bg : g_tx) + ')';
 }
 
 var defaultreplace = () => {
