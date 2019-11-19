8efd30f214f22aa65e0a9ef0529ac816b7d2d871
Bill Ni <bni@stuy.edu>
2019-11-19 17:16:18 -0500

    cleanup

---
+++ v2/src/components/sidebar_contents/sidebar_contents_default.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="sidebar-sticky">
+        <ul class="nav flex-column">
+            <button class="d-flex flex-row-reverse mr-1 a-hl" id="sblayout">
+                <li class="nav-item">
+                    <a class="nav-link a-txt-hl" href="#">
+                        <span data-feather="layout"></span>
+                        Layout &nbsp; <i class="fa fa-th-large"></i>
+                    </a>
+                </li>
+            </button>
+            <button class="d-flex flex-row-reverse mr-1 a-hl" id="sbmedia">
+                <li class="nav-item">
+                    <a class="nav-link a-txt-hl" href="#">
+                        <span data-feather="media"></span>
+                        Media &nbsp; <i class="fa fa-picture-o"></i>
+                    </a>
+                </li>
+            </button>
+            <button class="d-flex flex-row-reverse mr-1 a-hl" id="sbstyle">
+                <li class="nav-item">
+                    <a class="nav-link a-txt-hl" href="#">
+                        <span data-feather="style"></span>
+                        Style &nbsp; <i class="fa fa-font"></i>
+                    </a>
+                </li>
+            </button>
+            <button class="d-flex flex-row-reverse mr-1 a-hl" id="sbanim">
+                <li class="nav-item">
+                    <a class="nav-link a-txt-hl" href="#">
+                        <span data-feather="text"></span>
+                        Animation &nbsp; <i class="fa fa-rocket"></i>
+                    </a>
+                </li>
+            </button>
+            <button class="d-flex flex-row-reverse mr-1 a-hl" id="sbhelp">
+                <li class="nav-item">
+                    <a class="nav-link a-txt-hl" href="#">
+                        <span data-feather="text"></span>
+                        Help &nbsp; <i class="fa fa-question-circle"></i>
+                    </a>
+                </li>
+            </button>
+            <button class="d-flex flex-row-reverse mr-1 a-hl" id="sbprev">
+                <li class="nav-item">
+                    <a class="nav-link a-txt-hl" href="#">
+                        <span data-feather="prev"></span>
+                        Preview &nbsp; <i class="fa fa-eye"></i>
+                    </a>
+                </li>
+            </button>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'sidebar_contents_default'
+}
+</script>
---
+++ v2/src/components/sidebar_contents/sidebar_contents_media.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="sidebar-sticky">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'sidebar_contents_media'
+}
+</script>
