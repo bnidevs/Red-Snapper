e8b56deacbcaaf40242cadab6733d2a6c9a6439a
Bill Ni <bni@stuy.edu>
2019-11-24 01:49:42 -0500

    somewhat finished sidebar

--- v2/src/App.vue
+++ v2/src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <div class="container-fluid">
+      <nav class="col-2 d-none d-block bg-light sidebar" id="sbreplloc">
+        <sidebar_header nav_title="Red Snapper"/>
+        <sidebar_contents_default />
+      </nav>
+    </div>
   </div>
 </template>
 
@@ -7,21 +13,22 @@
 
 import sidebar_header from './components/sidebar_header.vue'
 
+import sidebar_contents_default from './components/sidebar_contents/sidebar_contents_default.vue'
+// import sidebar_contents_layout from './components/sidebar_contents/sidebar_contents_layout.vue'
+// import sidebar_contents_media from './components/sidebar_contents/sidebar_contents_media.vue'
+// import sidebar_contents_style from './components/sidebar_contents/sidebar_contents_style.vue'
+// import sidebar_contents_animation from './components/sidebar_contents/sidebar_contents_animation.vue'
+
 export default {
   name: 'app',
   components: {
-    sidebar_header
+    sidebar_header,
+    sidebar_contents_default
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './assets/css/sidebar.css';
+  @import './assets/css/frame.css';
 </style>
--- v2/src/components/sidebar_contents/sidebar_contents_style.vue
+++ v2/src/components/sidebar_contents/sidebar_contents_style.vue
@@ -1,28 +1,28 @@
 <template>
-    <div class="sidebar-sticky">
+	<div class="sidebar-sticky">
 	<ul class="nav flex-column">
 
 		<table class="ml-4">
 
 			<li class="nav-item d-flex flex-row">
-	      		<span data-feather="color_placement"></span>
-	      		<tr>
-	      			<td>
-	      				Background
-	      			</td>
-	      			<td>
+				<span data-feather="color_placement"></span>
+				<tr>
+					<td>
+						Background
+					</td>
+					<td>
 						<input type="color" id="bg_clr_view" value="#ffffff">
 					</td>
 				</tr>
 			</li>
 
 			<li class="nav-item d-flex flex-row">
-	      		<span data-feather="color_placement"></span>
-	      		<tr>
-	      			<td>
-	      				Text
-	      			</td>
-	      			<td>
+				<span data-feather="color_placement"></span>
+				<tr>
+					<td>
+						Text
+					</td>
+					<td>
 						<input type="color" id="bg_clr_view" value="#000000">
 					</td>
 				</tr>
